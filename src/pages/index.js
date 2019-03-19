02843fa39c31e4e6b5c31f581ac81b5b17e31a53
tomoyukikashiro <ksrtmyk@gmail.com>
2019-03-19 21:25:11 +0900

    feat(video): Show videos

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+import Divider from '@material-ui/core/Divider';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
@@ -34,6 +35,9 @@ const styles = theme => ({
       marginRight: 'auto',
     },
   },
+  gridContainer: {
+    padding: `${theme.spacing.unit * 4}px 0 ${theme.spacing.unit * 16}px`,
+  },
   cardGrid: {
     padding: `${theme.spacing.unit * 8}px 0`,
   },
@@ -54,10 +58,10 @@ const styles = theme => ({
   },
 });
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
 function IndexPage(props) {
   const { classes } = props;
+  const { data: { allVideo: { group } } } = props;
+  const versions = group.map(g => g.edges.map(e => e.node));
 
   return (
     <React.Fragment>
@@ -91,37 +95,39 @@ function IndexPage(props) {
             </div>
           </div>
         </div>
+        {/* End hero unit */}
         <div className={classNames(classes.layout, classes.cardGrid)}>
-          {/* End hero unit */}
-          <Grid container spacing={40}>
-            {cards.map(card => (
-              <Grid item key={card} sm={6} md={4} lg={3}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      View
-                    </Button>
-                    <Button size="small" color="primary">
-                      Edit
-                    </Button>
-                  </CardActions>
-                </Card>
+          {versions.map((videos, i) => (
+            <React.Fragment key={i}>
+              <Typography variant="h6" gutterBottom>
+                Version {videos[0].version}
+              </Typography>
+              <Divider />
+              <Grid container spacing={40} className={classes.gridContainer}>
+                {videos.map((video, k) => (
+                  <Grid item key={k} sm={6} md={4} lg={3}>
+                    <Card className={classes.card}>
+                      <CardMedia
+                        className={classes.cardMedia}
+                        image={`https://img.youtube.com/vi/${video.youtube_id}/maxresdefault.jpg`} // eslint-disable-line max-len
+                        title={video.function_name}
+                      />
+                      <CardContent className={classes.cardContent}>
+                        <Typography gutterBottom variant="subtitle1" component="h2">
+                          { video.function_name }
+                        </Typography>
+                      </CardContent>
+                      <CardActions>
+                        <Button size="small" color="primary">
+                          View
+                        </Button>
+                      </CardActions>
+                    </Card>
+                  </Grid>
+                ))}
               </Grid>
-            ))}
-          </Grid>
+            </React.Fragment>
+          ))}
         </div>
       </main>
       {/* Footer */}
@@ -155,6 +161,10 @@ export const query = graphql`
           node {
             function_name
             version
+            youtube_id
+            start
+            end
+            tags
           }
         }
       }
