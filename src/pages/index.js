7bf9ef287e7586137cc7bd2e6750850f94c77730
tomoyukikashiro <ksrtmyk@gmail.com>
2019-03-25 21:10:33 +0900

    style(main): chore layout tweak

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,11 +32,6 @@ const styles = theme => ({
     width: 'auto',
     marginLeft: theme.spacing.unit * 3,
     marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
-      width: 1100,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
   },
   gridContainer: {
     padding: `${theme.spacing.unit * 4}px 0 ${theme.spacing.unit * 16}px`,
